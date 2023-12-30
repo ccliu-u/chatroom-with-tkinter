0aeec55e8fd322e210038741c7c5ff71e2a147c9
ccliu-u <107825483+ccliu-u@users.noreply.github.com>
2023-12-30 21:48:32 +0800

    final

---
+++ source/create_mysql.py
@@ -0,0 +1,14 @@
+import pymysql
+
+Connection = pymysql.connect(host="localhost", user="root", password=" ", db="python_chat")  # 提交作业时密码隐去了
+
+cursor = Connection.cursor()
+sql_create_table = '''
+    create table user_information(
+      user_name varchar (20),
+      password varchar (20),
+      data LONGBLOB
+    )
+'''
+cursor.execute(sql_create_table)
+cursor.close()
